fd3248cd9fbff7b8db86b97cf220960a3a11bc44
stuartd <stuartd@gmail.com>
2017-09-07 00:36:49 +0100

    wtf formatting

--- src/get_files.rs
+++ src/get_files.rs
@@ -8,12 +8,12 @@ fn read_all_files(dir_path :&str) {
 	
 	for entry in fs::read_dir(dir_path).expect("Unable to list") {
         let entry = entry.expect("unable to get entry");
-		let path = entry.path();
+	let path = entry.path();
 
-		if path.is_dir() {
+	if path.is_dir() {
              read_all_files(path.as_os_str().to_str().unwrap());
         } 
-		else {
+	else {
              println!("{:?}", path);
         }
     }
